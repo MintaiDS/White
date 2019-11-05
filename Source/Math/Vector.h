a045fdb68b15937b1f963489b4bddffdde0b4012
Taras Makarevich <fpm.makareviTS@bsu.by>
2019-11-06 01:56:05 +0300

    add operator overloading

--- Source/Math/Vector.h
+++ Source/Math/Vector.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <vector>
 #include <memory>
 #include <algorithm>
@@ -10,20 +12,20 @@ struct Vector {
     Vector();
     Vector(std::size_t size, T* values);
     Vector(std::vector<T> values);
+    Vector(const Vector<T>& other);
 
     T& operator[](std::size_t index);
-    T& operator+=(const Vector<T>& other);
-    T& operator-=(const Vector<T>& other);
-    T& operator*=(const Vector<T>& other);
-    T& operator+=(const T value);
-    T& operator-=(const T value);
-    T& operator*=(const T value);
-    T operator+(const Vector<T>& other);
-    T operator-(const Vector<T>& other);
-    T operator*(const Vector<T>& other);
-    T operator+(const T value);
-    T operator-(const T value);
-    T operator*(const T value);
+    Vector& operator+=(const Vector<T>& other);
+    Vector& operator-=(const Vector<T>& other);
+    Vector& operator+=(const T value);
+    Vector& operator-=(const T value);
+    Vector& operator*=(const T value);
+    Vector operator+(const Vector<T>& other);
+    Vector operator-(const Vector<T>& other);
+    Vector operator+(const T value);
+    Vector operator-(const T value);
+    Vector operator*(const T value);
+    Vector dot(const Vector<T>& other);
 
     std::size_t size;
     std::unique_ptr<T[]> values;
@@ -51,8 +53,105 @@ Vector<T>::Vector(std::vector<T> values)
 }
 
 template<typename T>
+Vector<T>::Vector(const Vector<T>& other)
+        : size(other.size)
+        , values(std::make_unique<T[]>(size)) {
+    for (int i = 0; i < size; i++) {
+        this->values[i] = values[i];
+    }
+}
+
+template<typename T>
 T& Vector<T>::operator[](std::size_t index) {
     return values[index];
+}
+
+template<typename T>
+Vector<T>& Vector<T>::operator+=(const Vector<T>& other) {
+    for (int i = 0; i < size; ++i) {
+        values[i] += other[i];
+    }
+
+    return *this;
+}
+
+template<typename T>
+Vector<T>& Vector<T>::operator-=(const Vector<T>& other) {
+    for (int i = 0; i < size; ++i) {
+        values[i] -= other[i];
+    }
+
+    return *this;
+}
+
+template<typename T>
+Vector<T>& Vector<T>::operator+=(const T value) {
+    for (int i = 0; i < size; ++i) {
+        values[i] += value;
+    }
+
+    return *this;
+}
+
+template <typename T>
+Vector<T>& Vector<T>::operator-=(const T value) {
+    return *this += -value;
+}
+
+template<typename T>
+Vector<T>& Vector<T>::operator*=(const T value) {
+    for (int i = 0; i < size; i++) {
+        values[i] *= value;
+    }
+
+    return *this;
+}
+
+template<typename T>
+Vector<T> Vector<T>::operator+(const Vector<T>& other) {
+    Vector<T> ret(*this);
+    ret += other;
+
+    return ret;
+}
+
+template<typename T>
+Vector<T> Vector<T>::operator-(const Vector<T>& other) {
+    Vector<T> ret(*this);
+    ret -= other;
+
+    return ret;
+}
+
+template<typename T>
+Vector<T> Vector<T>::operator+(const T value) {
+    Vector<T> ret(*this);
+    ret += value;
+
+    return ret;
+}
+
+template<typename T>
+Vector<T> Vector<T>::operator-(const T value) {
+    return *this + -value;
+}
+
+template<typename T>
+Vector<T> Vector<T>::operator*(const T value) {
+    Vector<T> ret(*this);
+    ret *= value;
+
+    return ret;
+}
+
+template<typename T>
+Vector<T> Vector<T>::dot(const Vector<T>& other) {
+    T ret = 0;
+    for (int i = 0; i < size; i++) {
+        ret += *this[i] * other[i];
+    }
+
+    return ret;
 }
 
 }
