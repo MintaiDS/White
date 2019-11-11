4fd3174195567b9988ad98a87415cc23943511ff
Taras Makarevich <fpm.makareviTS@bsu.by>
2019-11-11 08:40:01 +0300

    add json parser and graph class

---
+++ Source/Util/Math/Graph.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <vector>
+#include <map>
+#include <string>
+
+class Edge {
+public:
+  Edge() {}
+  Edge(int idx, int length, int from, int to) : idx(idx), length(length)
+  {
+    v[0] = from;
+    v[1] = to;
+  }
+
+  int GetFrom() { return v[0]; }
+  int GetTo() { return v[1]; }
+  int GetLength() { return length; }
+
+private:
+  int idx;
+  int length;
+  int v[2];
+};
+
+class Vertex {
+
+public:
+  Vertex() {}
+  Vertex(int idx, int post_idx = -1) : idx(idx), post_idx(post_idx) {}
+
+  int GetIdx() { return idx; }
+
+  size_t GetId() { return id; }
+  void SetId(size_t id) { this->id = id; }
+
+  void AppendEdge(Edge* e) { edges.push_back(e); }
+
+  std::vector<Edge*>& GetEdgeList() { return edges; }
+
+private:
+  size_t id;
+  int idx;
+  int post_idx;
+  std::vector<Edge*> edges;
+};
+
+class Graph {
+public:
+  Graph() {}
+  Graph(int idx, std::string name) : idx(idx), name(name) {}
+  
+  void AppendEdge(Edge* e);
+  void AppendVertex(Vertex* v);
+
+  Vertex* GetVByIdx(int idx);
+
+private:
+  int idx;
+  std::string name;
+  std::vector<Edge*> edges;
+  std::vector<Vertex*> vertices;
+  std::map<int, size_t> v_ids;
+};
+
+Graph* ParseGraphFromJSON(std::string filename);
