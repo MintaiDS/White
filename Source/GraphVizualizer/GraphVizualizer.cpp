577be4962f86b7843531ffe541dc03bdfb9a5a67
Taras Makarevich <fpm.makareviTS@bsu.by>
2019-11-12 12:29:52 +0300

    implement simple graph vizualization without edges

--- Source/GraphVizualizer/GraphVizualizer.cpp
+++ Source/GraphVizualizer/GraphVizualizer.cpp
@@ -1,14 +1,68 @@
 #include "GraphVizualizer.h"
 #include "Renderer.h"
+#include "Rectangle.h"
+#include "Ring.h"
+#include "Disk.h"
 
 namespace White {
 namespace Engine {
 namespace Graphics {
 
-GraphVizualizer::GraphVizualizer(Renderer& renderer) : Game(renderer) {}
+GraphVizualizer::GraphVizualizer(Renderer& renderer) 
+        : Game(renderer) 
+        , graph(nullptr) {}
+
+void GraphVizualizer::LoadGraph(std::string path) {
+    graph = ParseGraphFromJSON(path);
+    Vector<GLfloat> cur = {-0.9f, 0.9};
+    Vector<GLfloat> moveRight = {0.4, 0.0};
+    Vector<GLfloat> moveDown = {0.0, -0.4};
+    for (int i = 0; i < 100; i++) {
+        Vertex* vertex = graph->GetVByIdx(i);
+        if (vertex) {
+            if (cur[0] > 0.9) {
+                cur[0] = -0.9;
+                cur += moveDown;
+            }
+            grid.push_back(cur);
+            cur += moveRight;
+        }
+    }
+    int curCell = 0;
+    for (int i = 0; i < 100; i++) {
+        Vertex* vertex = graph->GetVByIdx(i);
+        if (vertex) {
+            DisplayNode(grid[curCell++]);     
+        }
+    }
+}
+
+void GraphVizualizer::DisplayNode(Vector<GLfloat>& pos) {
+    Vector<GLfloat> color = {1.0f, 1.0f, 0.0f, 1.0f};
+    Vector<GLfloat> colorBorder = {0.0f, 0.0f, 1.0f, 1.0f};
+    //Util::Math::Rectangle<GLfloat> rect(5, 0.4);
+    //Mesh<GLfloat> rectMesh 
+    //    = rect.ToMesh(color + Vector<GLfloat>{-0.2, -0.2, 0.3, 0.0f}, 0);
+    //rectMesh.Scale({0.3f, 0.3f, 1.0f});
+    //rectMesh.Translate({0.6f, 0.6f, 0.3f});
+    //rectMesh.Rotate({0.0f, 0.0f, 20.0f});
+    Disk<GLfloat> disk(0.2);
+    Mesh<GLfloat> diskMesh = disk.ToMesh(color, 720);
+    diskMesh.Scale({0.2f, 0.2f, 1.0f});
+    diskMesh.Translate({pos[0], pos[1], 0.0f});
+    Ring<GLfloat> ring(0.1, 0.28);
+    Mesh<GLfloat> ringMesh = ring.ToMesh(colorBorder, 720);
+    ringMesh.Scale({0.2f, 0.2f, 1.0f});
+    ringMesh.Translate({pos[0], pos[1], 0.0f});
+    //AddMesh(rectMesh);
+    renderer.AddMesh(diskMesh);
+    renderer.AddMesh(ringMesh);
+}
 
 void GraphVizualizer::Play() {
-
+    if (!graph) {
+        LoadGraph("Temp/small_graph.json"); 
+    }
 }
 
 }
--- Source/GraphVizualizer/GraphVizualizer.h
+++ Source/GraphVizualizer/GraphVizualizer.h
@@ -1,6 +1,12 @@
 #pragma once
 
 #include "Game.h"
+#include "Graph.h"
+#include "Vector.h"
+
+#include <string>
+
+using namespace White::Util::Math;
 
 namespace White {
 namespace Engine {
@@ -10,7 +16,14 @@ class GraphVizualizer : Game {
 public:
     GraphVizualizer(Renderer& renderer);
 
+    void LoadGraph(std::string path);
+    void DisplayNode(Vector<GLfloat>& pos);
+
     virtual void Play();
+
+protected:
+    Graph* graph;
+    std::vector<Vector<GLfloat>> grid;
 };
 
 }
