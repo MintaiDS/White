402f57759b2a94ffb38cf51ca589046accaf77f3
Taras Makarevich <fpm.makareviTS@bsu.by>
2019-11-17 08:42:40 +0300

    fix script

--- build.bat
+++ build.bat
@@ -11,4 +11,5 @@ cd Build
 msbuild White.sln
 cd ..
 copy Build\Engine\Graphics\Core\Debug\Engine.exe Build\Bin\
-xcopy /e External\WGForgeTestData\src\WGForgeTestData Test\
+xcopy /e External\WGForgeTestData\src\WGForgeTestData
+ren Test test_graphs
