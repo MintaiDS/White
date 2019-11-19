a96dd5aa0d0e7e6b171c3fb3a82ba16628004208
Taras Makarevich <fpm.makareviTS@bsu.by>
2019-11-19 23:22:19 +0300

    Code style improved.

--- Source/Util/Math/Connection.h
+++ Source/Util/Math/Connection.h
@@ -14,78 +14,65 @@
 
 namespace White {
 namespace Engine {
-namespace Graphics {
 
-enum Action
-{
-  LOGIN = 1,
-  LOGOUT = 2,
-  MOVE = 3,
-  UPGRADE = 4,
-  TURN = 5,
-  PLAYER = 6,
-  GAMES = 7,
-  MAP = 10
+enum Action {
+    LOGIN = 1,
+    LOGOUT = 2,
+    MOVE = 3,
+    UPGRADE = 4,
+    TURN = 5,
+    PLAYER = 6,
+    GAMES = 7,
+    MAP = 10
 };
 
-enum Result
-{
-  OKEY = 0,
-  BAD_COMMAND = 1,
-  RESOURCE_NOT_FOUND = 2,
-  ACCESS_DENIED = 3,
-  INAPPROPRIATE_GAME_STATE = 4,
-  TIMEOUT = 5,
-  INTERNAL_SERVER_ERROR = 500
+enum Result {
+    OKEY = 0,
+    BAD_COMMAND = 1,
+    RESOURCE_NOT_FOUND = 2,
+    ACCESS_DENIED = 3,
+    INAPPROPRIATE_GAME_STATE = 4,
+    TIMEOUT = 5,
+    INTERNAL_SERVER_ERROR = 500
 };
 
-struct ActionMessage
-{
-  Action actionCode;
-  size_t dataLength;
-  char* data;
+struct ActionMessage {
+    Action actionCode;
+    size_t dataLength;
+    char* data;
 };
 
-struct ResponseMessage
-{
-  Result result;
-  size_t dataLength;
-  char* data;
+struct ResponseMessage {
+    Result result;
+    size_t dataLength;
+    char* data;
 };
 
-class Connection
-{
+class Connection {
 public:
-  const char* LAYER0 = "{\"layer\":0}";
-  const char* LAYER1 = "{\"layer\":1}";
-  const char* LAYER10 = "{\"layer\":10}";
+    const char* LAYER0 = "{\"layer\":0}";
+    const char* LAYER1 = "{\"layer\":1}";
+    const char* LAYER10 = "{\"layer\":10}";
   
-  static Connection& GetInstance(const char* hostname, int port)
-  {
-    static Connection instance(hostname, port);
-    return instance;
-  }
+    static Connection& GetInstance(const char* hostname, int port) {
+        static Connection instance(hostname, port);
 
-  bool Request(const ActionMessage& msg, ResponseMessage& resp);
-  ActionMessage FormActionMessage(Action a, std::string data);
-  //bool Reconnect();
+        return instance;
+    }
+
+    bool Request(const ActionMessage& msg, ResponseMessage& resp);
+    ActionMessage FormActionMessage(Action a, std::string data);
   
 private:
-  SOCKET clientSocket = SOCKET_ERROR;
-
-  Connection(const char* hostname, int port) {
-    Connect(hostname, port);
-  }
-  Connection(const Connection& );
-  Connection& operator= (Connection& );
-  ~Connection();
-  bool Connect(const char* hostname, int port);
-  
+    SOCKET clientSocket = SOCKET_ERROR;
+    Connection(const char* hostname, int port) {
+        Connect(hostname, port);
+    }
+    Connection(const Connection& );
+    Connection& operator= (Connection& );
+    ~Connection();
+    bool Connect(const char* hostname, int port); 
 };
 
 }
 }
-}
-
-
-
