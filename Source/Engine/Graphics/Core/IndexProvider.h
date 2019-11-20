237044cfc17005625cee116981be1638bbe99c92
Taras Makarevich <fpm.makareviTS@bsu.by>
2019-11-21 00:56:29 +0300

    Add indexation helper class.

---
+++ Source/Engine/Graphics/Core/IndexProvider.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <deque>
+#include <map>
+#include <iterator>
+
+namespace White {
+namespace Engine {
+
+template<typename T = unsigned, typename U = unsigned>
+class IndexProvider {
+public:
+    void Add(T key, U index);
+    void Remove(T key);
+    U Get(T key);
+
+protected:
+    std::map<T, U> mapping;
+    std::deque<U> unused;
+};
+
+template<typename T = unsigned, typename U = unsigned>
+void IndexProvider<T, U>::Add(T key, U index) {
+    if (!unused.empty()) {
+        index = unused.front();
+        unused.pop_front();
+    }
+
+    mapping.insert(std::make_pair(key, index));
+}
+
+template<typename T = unsigned, typename U = unsigned>
+void IndexProvider<T, U>::Remove(T key) {
+    std::map<T, U>::iterator it = mapping.find(key);
+    U index = it->second;
+    mapping.erase(it);
+    unused.push_back(index);
+}
+
+template<typename T = unsigned, typename U = unsigned>
+U IndexProvider<T, U>::Get(T key) { 
+    std::map<T, U>::iterator it = mapping.find(key);
+    
+    return it->second;
+}
+
+}
+}
