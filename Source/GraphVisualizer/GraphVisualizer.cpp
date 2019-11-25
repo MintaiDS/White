1e50f29eb7f97662396bfdd67dbf768b6f0e9553
Taras Makarevich <fpm.makareviTS@bsu.by>
2019-11-26 02:31:29 +0300

    Restore visualizer working

--- Source/GraphVisualizer/GraphVisualizer.cpp
+++ Source/GraphVisualizer/GraphVisualizer.cpp
@@ -58,35 +58,11 @@ void GraphVisualizer::LoadGraph(std::string name) {
     }
     delete[](resp.data);
     delete[](msg.data);
-   /* data = "{\"layer\":10}";
-    msg = conn.FormActionMessage(Action::MAP, data);
-    conn.Request(msg, resp);
-    delete[](resp.data);
-    if (resp.result == Result::OKEY)
-    {
-      ParseCoordFromJSON(gr, resp.data);
-    }*/
-    //msg = conn.FormActionMessage(Action::LOGOUT, "");
-    //conn.Request(msg, resp);
     //graph.reset(&gr);
 }
 
 void GraphVisualizer::UpdateCamera() {
     Program& program = renderer.GetProgram();
-
-    // Setup view matrix.
-    //Matrix<GLfloat> view = Matrix<GLfloat>::Identity(2);
-    //view[0][0] *= (grid->gridSize[1] * grid->cellSize[0]) / 2.0f;
-    //view[1][1] *= (grid->gridSize[1] * grid->cellSize[0]) / 2.0f;
-    //view.Inverse();
-    //view = {{view[0][0], view[0][1], 0.0f, 0.0f}, 
-    //        {view[1][0], view[1][1], 0.0f, 0.0f}, 
-    //        {0.0f, 0.0f, 1.0f, 0.0f}, 
-    //        {0.0f, 0.0f, 0.0f, 1.0f}};
-    //camera.Translate({0.0f, 0.0f, -0.1f});
-    //camera.Rotate({0.0f, 0.0f, -20.0f});
-    //camera.Scale({1.1f, 1.1f, 1.1f});
-
 
     Matrix<GLfloat> view = camera.GetViewMatrix();
     std::unique_ptr<GLfloat[]> raw 
@@ -103,13 +79,10 @@ void GraphVisualizer::UpdateCamera() {
 
     GLfloat scaleFactor = (grid->gridSize[1] * grid->cellSize[0]) / 2.0f;
 
-    // Setup projection matrix.
-    //glDisable(GL_CULL_FACE);
     Matrix<GLfloat> projection = Matrix<GLfloat>::Identity(4);
     projection
         = Matrix<GLfloat>::Projection(-0.1f, 0.1f, 
                                       0.1f, -0.1f, 0.1f, 1000.0f);
-    //projection *= -1;
     for (int i = 0; i < projection.rows; i++) {
         for (int j = 0; j < projection.columns; j++) {
             raw.get()[i * projection.columns + j] = projection[i][j];
@@ -136,8 +109,6 @@ void GraphVisualizer::Play() {
         // LoadGraph(path); 
         LoadGraph(name); 
         int verticesCnt = graph->GetVerticesCnt();
-        //int dimension = (std::log2(verticesCnt) - 1) 
-        //                * (std::sqrt(verticesCnt) + 1);
         int dimension = 2 * (std::sqrt(verticesCnt) + 1); 
         grid.reset(new Grid({0.0f, 0.0f}, 
                             {dimension, dimension}, 
@@ -291,12 +262,6 @@ void GraphVisualizer::Play() {
         renderer.UpdateVertexData();
         UpdateCamera();
     }
-
-    //White::Util::Logger& logger = White::Util::Logger::GetInstance();
-    //logger.Init("log.txt");
-    //logger << "Log : ";
-    //logger << 1.1f << 2;
-
     InterfaceProvider ip;
     if (mode == 1) {
         if ((GetAsyncKeyState(VK_LEFT) < 0) != keys[0]) {
@@ -364,19 +329,16 @@ void GraphVisualizer::Play() {
             keys[4] = -keys[4];
             camera.Rotate({0.0f, -2.0f, 0.0f});
             UpdateCamera();
-
         } 
         if ((GetAsyncKeyState(0x57) < 0) != keys[5]) {
             keys[5] = -keys[5];
             camera.Rotate({2.0f, 0.0f, 0.0f});
             UpdateCamera();
-
         }
         if ((GetAsyncKeyState(0x44) < 0) != keys[6]) {
             keys[6] = -keys[6];
             camera.Rotate({0.0f, 2.0f, -0.0f});
             UpdateCamera();
-
         }
         if ((GetAsyncKeyState(0x53) < 0) != keys[7]) {
             keys[7] = -keys[7];
@@ -393,18 +355,12 @@ void GraphVisualizer::Play() {
             keys[10] = -keys[10];
             camera.Rotate({-0.0f, 0.0f, -2.0f});
             UpdateCamera();
-
-        }
- 
+        } 
     }
-
     if ((GetAsyncKeyState(0x4D) < 0) != keys[8]) {
         keys[8] = -keys[8];
         mode = (mode + 1) % 2;
     }
-
-        //ip.Query<IRotatable>(cubeId)->Rotate<float>({0.1f, 0.0f, 0.1f}); 
-    //ip.Query<ITranslatable>(cubeId)->Translate<float>({0.0f, 0.0f, 0.0008f}); 
 }
 
 }
