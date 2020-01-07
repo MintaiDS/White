3676d102c3f03271fd1c57ee607716f6c5d7e939
Taras Makarevich <fpm.makareviTS@bsu.by>
2020-01-08 01:19:22 +0300

    Code cleanup in mesh class.

--- Source/Engine/Graphics/Drawing/Mesh.h
+++ Source/Engine/Graphics/Drawing/Mesh.h
@@ -145,15 +145,6 @@ void Mesh<T, U>::ToggleTransform() {
     isTransformed = !isTransformed;    
 }
 
-//template<typename T, typename U = unsigned>
-//Mesh<T, U> Mesh<T, U>::CreateFromShape(
-//                        const Segment<T>& segment, const Vector<T>& color) {
-//    std::vector<VertexData<T>> vertices;    
-//    Mesh<T, U> ret(vertices);
-//
-//    return ret;
-//}
-
 template<typename T, typename U = unsigned>
 std::size_t Mesh<T, U>::GetSize() const {
     return vertices.size() * vertices[0].GetSize();
@@ -177,19 +168,16 @@ Matrix<T> Mesh<T, U>::GetRotationMatrix() const {
 
 template<typename T, typename U = unsigned>
 Matrix<T> Mesh<T, U>::GetScalingMatrix() const {
-    //UpdateTransformationMatrix();
     return Matrix::Scaling(scaling);
 }
 
 template<typename T, typename U = unsigned>
 Matrix<T> Mesh<T, U>::GetTranslationMatrix() const {
-    //UpdateTransformationMatrix();
     return Matrix::Translation(translation);
 }
 
 template<typename T, typename U = unsigned>
 Vector<T> Mesh<T, U>::GetRotation() const {
-    //UpdateTransformationMatrix();
     Vector<T> ret(rotation);
 
     return ret;
@@ -197,7 +185,6 @@ Vector<T> Mesh<T, U>::GetRotation() const {
 
 template<typename T, typename U = unsigned>
 Vector<T> Mesh<T, U>::GetScaling() const {
-    //UpdateTransformationMatrix();
     Vector<T> ret(scaling);
 
     return ret;
@@ -205,7 +192,6 @@ Vector<T> Mesh<T, U>::GetScaling() const {
 
 template<typename T, typename U = unsigned>
 Vector<T> Mesh<T, U>::GetTranslation() const {
-    //UpdateTransformationMatrix();
     Vector<T> ret(translation);
 
     return ret;
@@ -215,7 +201,6 @@ template<typename T, typename U = unsigned>
 void Mesh<T, U>::Scale(const Vector<float>& scaling) {
     this->scaling = scaling; 
     isTransformed = true;
-    //UpdateTransformationMatrix();
     UpdateTransformationMatrix(transformation.parental);
 }
 
@@ -223,7 +208,6 @@ template<typename T, typename U = unsigned>
 void Mesh<T, U>::Rotate(const Vector<float>& rotation) {
     this->rotation += rotation;
     isTransformed = true;
-    //UpdateTransformationMatrix();
     UpdateTransformationMatrix(transformation.parental);
 }
 
@@ -231,7 +215,6 @@ template<typename T, typename U = unsigned>
 void Mesh<T, U>::Translate(const Vector<float>& translation) {
     this->translation += translation;
     isTransformed = true;
-    //UpdateTransformationMatrix();
     UpdateTransformationMatrix(transformation.parental);
 }
 
@@ -243,14 +226,12 @@ void Mesh<T, U>::Transform(const Vector<float>& scaling,
     Rotate(rotation);
     Translate(translation);
     isTransformed = true;
-    //UpdateTransformationMatrix();
     UpdateTransformationMatrix(transformation.parental);
 }
 
 template<typename T, typename U = unsigned>
 Matrix<float>& Mesh<T, U>::GetTransformationMatrix() {
-    //UpdateTransformationMatrix();
-    return transformation.that;//transformationMatrix;
+    return transformation.that;
 }
 
 template<typename T, typename U = unsigned>
@@ -276,23 +257,6 @@ void Mesh<T, U>::UpdateTransformationMatrix() {
 
 template<typename T, typename U = unsigned>
 void Mesh<T, U>::UpdateTransformationMatrix(Matrix<float> parental) {
-    //Matrix<float> scalingMatrix = Matrix<float>::Scaling(scaling);
-    //Matrix<float> rotationMatrix = Matrix<float>::Rotation(rotation); 
-    //Matrix<float> translationMatrix = Matrix<float>::Translation(translation);
-    //scalingMatrix 
-    //    = {{scalingMatrix[0][0], scalingMatrix[0][1], scalingMatrix[0][2], 0},
-    //       {scalingMatrix[1][0], scalingMatrix[1][1], scalingMatrix[1][2], 0}, 
-    //       {scalingMatrix[2][0], scalingMatrix[2][1], scalingMatrix[2][2], 0},
-    //       {0, 0, 0, 1.0f}};
-    //rotationMatrix 
-    //    = {{rotationMatrix[0][0], 
-    //        rotationMatrix[0][1], rotationMatrix[0][2], 0},
-    //       {rotationMatrix[1][0], 
-    //        rotationMatrix[1][1], rotationMatrix[1][2], 0}, 
-    //       {rotationMatrix[2][0], 
-    //        rotationMatrix[2][1], rotationMatrix[2][2], 0},
-    //       {0, 0, 0, 1.0f}};
-    //transformationMatrix = translationMatrix * rotationMatrix * scalingMatrix;
     UpdateTransformationMatrix();
     transformation.parental = parental;
     transformation.that = parental * transformationMatrix;
